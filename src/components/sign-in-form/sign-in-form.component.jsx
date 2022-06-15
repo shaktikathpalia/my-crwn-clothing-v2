6c687db979e3480127c038aaf99b799101226dc4
Shakti Kathpalia <shakti@elayers.net>
2022-06-15 17:45:25 +0530

    signout

--- src/components/sign-in-form/sign-in-form.component.jsx
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -29,6 +29,7 @@ const SignInForm = () => {
   const signInWithGoogle = async () => {
     const { user } = await signInWithGooglePopup();
     await createUserDocumentFromAuth(user);
+    setCurrentUser(user);
   };
 
   const handleSubmit = async (event) => {
--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -4,13 +4,19 @@ import { Outlet, Link } from 'react-router-dom';
 import { ReactComponent as CrwnLogo } from '../../assets/crown.svg';
 import { UserContext } from '../../contexts/user.context';
 
+import { signOutUser } from '../../utils/firebase/firebase.utils';
+
 import './navigation.styles.scss';
 
 const Navigation = () => {
   
-  const { currentUser } = useContext(UserContext);
-  console.log(currentUser);
+  const { currentUser, setCurrentUser } = useContext(UserContext);
 
+  const logoutHandler = async () => {
+    await signOutUser();
+    setCurrentUser(null);
+  }
+console.log(currentUser);
   return (
     <Fragment>
       <div className='navigation'>
@@ -21,9 +27,22 @@ const Navigation = () => {
           <Link className='nav-link' to='/shop'>
             SHOP
           </Link>
-          <Link className='nav-link' to='/auth'>
-            SIGN IN
-          </Link>
+          {
+            currentUser ? (
+              <>
+              {
+                currentUser.displayName && (
+                  <span>Welcome, {currentUser.displayName}</span>
+                )
+              }
+                <span className='nav-link' onClick={logoutHandler}>SIGN OUT</span>
+              </>
+            ) : (
+              <Link className='nav-link' to='/auth'>
+                SIGN IN
+              </Link>
+            )
+          }
         </div>
       </div>
       <Outlet />
