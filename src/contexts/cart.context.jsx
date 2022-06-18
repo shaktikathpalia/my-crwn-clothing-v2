23600940366a3fa07341e271dafe9faa9f656790
Shakti Kathpalia <shakti@elayers.net>
2022-06-18 10:03:17 +0530

    add to cart

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,14 +1,31 @@
 import { createContext, useState } from 'react';
 
+const addCartItem = (cartItems, productToAdd) => {
+  const itemIdx = cartItems.findIndex(item => item.id === productToAdd.id);
+  if(itemIdx > -1){
+    cartItems[itemIdx].quantity += 1;
+  }else{
+    cartItems.push({ ...productToAdd, quantity: 1 });
+  }
+  return cartItems;
+}
+
 export const CartContext = createContext({
   isCartOpen: false,
   setIsCartOpen: () => null,
+  cartItems: [],
+  addItemToCart: () => null,
 })
 
 export const CartProvider = ({ children }) => {
 
   const [isCartOpen, setIsCartOpen ] = useState(false);
-  
+  const [cartItems, setCartItems] = useState([]);
 
-  return <CartContext.Provider value={{ isCartOpen, setIsCartOpen }}>{ children }</CartContext.Provider>
+  const addItemToCart = (productToAdd) => {
+    setCartItems(addCartItem(cartItems, productToAdd));
+  }
+
+
+  return <CartContext.Provider value={{ isCartOpen, setIsCartOpen, cartItems, addItemToCart }}>{ children }</CartContext.Provider>
 }
